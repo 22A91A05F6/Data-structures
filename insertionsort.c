c616fcfa10321078174cff56c22b10f2e3b8ac2f
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2023-05-08 12:20:36 +0530

    Add files via upload

---
+++ insertionsort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>     //insertion sort
+void selection_sort(int *arr,int n)
+{
+	int i,j,k;
+	k=arr[i];
+	for(i=0;i<n;i++)
+	{
+		
+	}
+	
+	  
+	  
+	                                  
+	                                            
+}
+int main()
+{
+	int i,n,arr[100];
+	printf("Enter the size of an array");
+	scanf("%d",&n);
+	printf("Enter the elements in an array");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	selection_sort(arr,n);
+	for(i=0;i<n;i++)
+	{
+		
+		printf("%d ",arr[i]);
+	}
+	
+}
